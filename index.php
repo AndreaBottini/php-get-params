4d1ce03dea4082da17a0cdc61a177a6166e75ef7
Andrea Bottini <andrea.bottini22@gmail.com>
2020-02-21 16:20:20 +0100

    Primo esercizio

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
-
-
-
+// Stampare una stringa verde se la variabile password passata in GET è uguale a “Boolean”, altrimenti stampare una stringa rossa.
+$password = $_GET["password"];
+if ($password == 'Boolean') {
+  $message = 'Hai Vinto!';
+  $color = "green";
+}
+else {
+  $message = 'Hai Perso!';
+  $color = "red";
+}
  ?>
 
  <!DOCTYPE html>
  <html lang="en">
  <head>
    <meta charset="UTF-8">
+   <style media="screen">
+     .green {
+       color: green;
+     }
+     .red {
+       color: red;
+     }
+   </style>
+   <link rel="stylesheet" href="style.css">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Document</title>
  </head>
  <body>
+   <h1 class="<?php echo $color; ?>"> <?php echo $message; ?>
 
+   </h1>
  </body>
  </html>
